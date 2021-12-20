6b46628f0a59fe4d095f3390f7a4d85f79c957a5
Omkar Raut <omkarraut2412@gmail.com>
2021-12-20 19:41:59 +0530

    updated

---
+++ src/components/pages/Home.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [users, setUser] = useState([]);
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:3003/users");
+    setUser(result.data.reverse());
+  };
+
+  const deleteUser = async id => {
+    await axios.delete(`http://localhost:3003/users/${id}`);
+    loadUsers();
+  };
+
+  return (
+    <div className="container">
+      <div className="py-4">
+        
+        <table class="table border shadow">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email ID</th>
+              <th scope="col">Contact</th>
+              <th scope="col">Branch</th>
+              <th>Action</th>
+            </tr>
+            <tr>
+              <th scope="col">1</th>
+              <th scope="col">Omkar raut</th>
+              <th scope="col">omkarraut2412@gmail.com</th>
+              <th scope="col">8433830474</th>
+              <th scope="col">IT</th>
+              {/* <th>Action</th> */}
+              <td>
+                  <Link class="btn btn-primary mr-2" 
+                  // to={`/users/${user.id}`}
+                  >
+                    View
+                  </Link>
+                  <Link
+                    class="btn btn-outline-primary mr-2"
+                    // to={`/users/edit/${user.id}`}
+                  >
+                    Update
+                  </Link>
+                  <Link
+                    class="btn btn-danger"
+                    // onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </Link>
+                </td>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{user.name}</td>                
+                <td>{user.email}</td>
+                <td>{user.contact}</td>
+                <td>{user.branch}</td>
+                <td>
+                  <Link class="btn btn-primary mr-2" to={`/users/${user.id}`}>
+                    View
+                  </Link>
+                  <Link
+                    class="btn btn-outline-primary mr-2"
+                    to={`/users/edit/${user.id}`}
+                  >
+                    Update
+                  </Link>
+                  <Link
+                    class="btn btn-danger"
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/users/AddUser.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import axios from 'axios'
+import { useHistory } from "react-router-dom";
+
+const AddUser = () => {
+  let history = useHistory();
+  const [user, setUser] = useState({
+    name: "",
+    email: "",  
+    contact: "",
+    branch: ""
+  });
+
+  const { name, email, contact, branch } = user;
+  const onInputChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    await axios.post("http://localhost:3003/users", user);
+    history.push("/");
+  };
+  return (
+    <div className="container">
+      <div className="w-75 mx-auto shadow p-5">
+        <h2 className="text-center mb-4">Add Student</h2>
+        <form onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Name"
+              name="name"
+              value={name}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control form-control-lg"
+              placeholder="Enter Your E-mail Address"
+              name="email"
+              value={email}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your contact Number"
+              name="contact"
+              value={contact}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your branch"
+              name="branch"
+              value={branch}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <button className="btn btn-primary btn-block">Add Student</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddUser;
